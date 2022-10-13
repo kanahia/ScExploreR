9f3fb876a99d42392c3c1938aec3e380bc86a3b8
Migdal <mcjmigdal@gmail.com>
2022-10-13 15:13:08 +0200

    add full references to imporeted libraries when using functions

--- inst/ScExploreR/app.R
+++ inst/ScExploreR/app.R
@@ -8,14 +8,14 @@ devtools::load_all()
 
 
   ## all_markers
-   markers_all <- read_multiple_sheets(file = "/home/jason/data/shiny_dashboard/heart10x/data/main_clustering_markers.xlsx")
+   markers_all <- ScExploreR::read_multiple_sheets(file = "/home/jason/data/shiny_dashboard/heart10x/data/main_clustering_markers.xlsx")
    markers_all <- markers_all %>%
      dplyr::mutate(avg_log2FC = round(avg_log2FC, digits = 2)) %>%
      dplyr::mutate(p_val_adj = format.pval(p_val_adj, digits = 2)) %>%
      dplyr::mutate(p_val = format.pval(p_val, digits = 2))
 
   ## markers_myo
-   markers_myo <- read_multiple_sheets("/home/jason/data/10x_heart/SCT_int_no_regression/Outdir/Plots/Myocardium/markers_myo_new-updated.xlsx")
+   markers_myo <- ScExploreR::read_multiple_sheets("/home/jason/data/10x_heart/SCT_int_no_regression/Outdir/Plots/Myocardium/markers_myo_new-updated.xlsx")
    markers_myo <- markers_myo %>%
      dplyr::mutate(avg_log2FC = round(avg_log2FC, digits = 2)) %>%
      dplyr::mutate(p_val_adj = format.pval(p_val_adj, digits = 2)) %>%
@@ -45,8 +45,8 @@ devtools::load_all()
      metadata_all %>%
      dplyr::select(edited_res.1.5, UMAP_1, UMAP_2) %>%
      dplyr::group_by(edited_res.1.5) %>%
-     summarise(u1 = median(UMAP_1),
-               u2 = median(UMAP_2)) %>%
+     dplyr::summarise(u1 = median(UMAP_1),
+                      u2 = median(UMAP_2)) %>%
      dplyr::rename("label" = 1) %>%
      dplyr::mutate(color = colors_main_umap)
 
@@ -59,97 +59,98 @@ title <- tags$a(href='https://zdglab.iimcb.gov.pl/',
                          align = "center"),
                 target="_blank")
 
-ui <- dashboardPage(
+ui <- shinydashboard::dashboardPage(
   skin = "blue",
-  dashboardHeader(title = title,
+  shinydashboard::dashboardHeader(
+                  title = title,
                   titleWidth = 220,
                   tags$li(class = "dropdown",
-                          tags$style(".main-header {max-height: 100px}"),
-                          tags$style(".main-header .logo {height: 100px;}"),
-                          tags$style(".sidebar-toggle {height: 100px; padding-top: 20px !important;}"),
-                          tags$style(".navbar {min-height:100px; !important}")
+                          shiny::tags$style(".main-header {max-height: 100px}"),
+                          shiny::tags$style(".main-header .logo {height: 100px;}"),
+                          shiny::tags$style(".sidebar-toggle {height: 100px; padding-top: 20px !important;}"),
+                          shiny::tags$style(".navbar {min-height:100px; !important}")
                           )),
-  dashboardSidebar(width = 220,
-                   sidebarMenu(
-                     menuItem("Blank", tabName = "blank", icon = icon("dashboard")),
-                     menuItem("Menu"),
-                     menuItem("Whole heart", tabName = "whole_heart", icon = icon("heart", lib = "glyphicon"),
-                              menuSubItem("Gene table", tabName = "gene_table_all"),
-                              menuSubItem("Visualization", tabName = "visualization_all")),
-                     menuItem("Myocardium", tabName = "myocardium", icon = icon("triangle-top", lib = "glyphicon"),
-                              menuSubItem("Gene table", tabName = "gene_table_myo"),
-                              menuSubItem("Visualization", tabName = "visualization_myo"))
+  shinydashboard::dashboardSidebar(
+                   width = 220,
+                   shinydashboard::sidebarMenu(
+                     shinydashboard::menuItem("Blank", tabName = "blank", icon = icon("dashboard")),
+                     shinydashboard::menuItem("Menu"),
+                     shinydashboard::menuItem("Whole heart", tabName = "whole_heart", icon = icon("heart", lib = "glyphicon"),
+                              shinydashboard::menuSubItem("Gene table", tabName = "gene_table_all"),
+                              shinydashboard::menuSubItem("Visualization", tabName = "visualization_all")),
+                     shinydashboard::menuItem("Myocardium", tabName = "myocardium", icon = icon("triangle-top", lib = "glyphicon"),
+                              shinydashboard::menuSubItem("Gene table", tabName = "gene_table_myo"),
+                              shinydashboard::menuSubItem("Visualization", tabName = "visualization_myo"))
                      )
                    # Adjust the sidebar
                    #tags$style(".left-side, .main-sidebar {padding-top: -20px}")
                    ),
-  dashboardBody(
-    tabItems(
-      tabItem("whole_heart"),
-      tabItem("myocardium"),
-      tabItem("visualization_all",
-              tabPanel("Visualization",
-                    wellPanel(
-                       fluidRow(#column(width = 2),
-                                column(width = 2, offset = 6,
-                                       selectInput(inputId = "type_of_data",
+  shinydashboard::dashboardBody(
+    shinydashboard::tabItems(
+      shinydashboard::tabItem("whole_heart"),
+      shinydashboard::tabItem("myocardium"),
+      shinydashboard::tabItem("visualization_all",
+              shinydashboard::tabPanel("Visualization",
+                    shinydashboard::wellPanel(
+                       shiny::fluidRow(#column(width = 2),
+                                shiny::column(width = 2, offset = 6,
+                                       shiny::selectInput(inputId = "type_of_data",
                                                    label = "Type of data",
                                                    choices = c("Overview", "Dataset", "Line",
                                                                "Stage", "Percent.mt", "Percent.hemoglobin",
                                                                "Cell-Cycle-phase", "nCount_RNA", "nFeature_RNA", "iris"),
                                                    selected = "Overview"))
                                 ),
-                        fluidRow(column(width = 5,
-                                        plotOutput("DimPlot", width = "100%")), #, width = 800, height = 600)),
-                                 column(width = 5, offset = 1,
-                                        plotOutput("Multiplot", width = "100%")) # width = 800, height = 600))
+                        shiny::fluidRow(shiny::column(width = 5,
+                                        shiny::plotOutput("DimPlot", width = "100%")), #, width = 800, height = 600)),
+                                 shiny::column(width = 5, offset = 1,
+                                        shiny::plotOutput("Multiplot", width = "100%")) # width = 800, height = 600))
                                 )
                        ),
-                    wellPanel(
-                       fluidRow(column(width = 2,
-                                       textInput(inputId = "gene",
+                    shiny::wellPanel(
+                       shiny::fluidRow(shiny::column(width = 2,
+                                       shiny::textInput(inputId = "gene",
                                                  label = "Choose gene",
                                                  value = "myh6",
                                                  placeholder = "myh6",
                                                  width = "100px")),
-                                column(width = 2, offset = 4, style= 'padding-top:0px;',
-                                  textInput(inputId = "gene2",
+                                shiny::column(width = 2, offset = 4, style= 'padding-top:0px;',
+                                  shiny::textInput(inputId = "gene2",
                                             label = "Choose gene",
                                             value = "myh7l",
                                             placeholder = "myh7l",
                                             width = "100px"))
                                 ),
-                       fluidRow(column(width = 5,
-                                       plotOutput("Featureplot", width = "100%")), # width = 800, height = 600)),
-                                column(width = 5,
+                       shiny::fluidRow(shiny::column(width = 5,
+                                       shiny::plotOutput("Featureplot", width = "100%")), # width = 800, height = 600)),
+                                shiny::column(width = 5,
                                        offset = 1,
-                                       plotOutput("my_FT", width = "100%")), ## width = 800, height = 600)),
+                                       shiny::plotOutput("my_FT", width = "100%")), ## width = 800, height = 600)),
                                 style = "background-color: gray99;"
-
                                 )
                        )
                     )
               ),
-      tabItem("gene_table_all",
-              fluidPage(
-                h1("Gene table: Whole heart")),
-              dataTableOutput("markers_all")
+      shinydashboard::tabItem("gene_table_all",
+              shiny::fluidPage(
+                shiny::h1("Gene table: Whole heart")),
+              shiny::dataTableOutput("markers_all")
               ),
-      tabItem("visualization_myo", "Sub-item 1 tab content"),
-      tabItem("gene_table_myo",
-              fluidPage(
-                h1("Gene table: Myocardium")),
-              dataTableOutput("markers_myo"))
+      shinydashboard::tabItem("visualization_myo", "Sub-item 1 tab content"),
+      shinydashboard::tabItem("gene_table_myo",
+              shiny::fluidPage(
+                shiny::h1("Gene table: Myocardium")),
+              shiny::dataTableOutput("markers_myo"))
       )
     )
   )
 
 
 server <- function(input, output) {
-  output$markers_all <- renderDataTable(markers_all, options = list(iDisplayLength = 10))
-  output$markers_myo <- renderDataTable(markers_myo, options = list(iDisplayLength = 10))
+  output$markers_all <- shiny::renderDataTable(markers_all, options = list(iDisplayLength = 10))
+  output$markers_myo <- shiny::renderDataTable(markers_myo, options = list(iDisplayLength = 10))
    output$Featureplot <-
-     renderPlot({
+     shiny::renderPlot({
         my_FeaturePlot(metadata = metadata_all,
                        data_slot = slot_data_all,
                        gene = c(input$gene),
@@ -160,7 +161,7 @@ server <- function(input, output) {
       })
 
   output$my_FT <-
-    renderPlot({
+    shiny::renderPlot({
       my_FeaturePlot(metadata = metadata_all,
                      data_slot = slot_data_all,
                      gene = c(input$gene2),
@@ -170,23 +171,23 @@ server <- function(input, output) {
       })
 
   output$DimPlot <-
-    renderPlot({
-      ggplot(metadata_all) +
-        geom_point(aes(x = UMAP_1 ,
+    shiny::renderPlot({
+      ggplot2::ggplot(metadata_all) +
+        ggplot2::geom_point(ggplot2::aes(x = UMAP_1 ,
                        y = UMAP_2,
                        color = edited_res.1.5),
                    alpha = 0.9,
                    size = 0.04) +
-        theme_minimal() +
-        one_theme +
-        NoLegend() +
+        ggplot2::theme_minimal() +
+        ggplot2::one_theme +
+        ggplot2::NoLegend() +
         # annotate(geom = "text",
         #          label = labels_all$label  %>% stringr::str_wrap(., width = 10),
         #          x = labels_all$u1,
         #          y = labels_all$u2,
         #          size = 4,
         #          lineheight = 0.8) +
-        scale_color_manual(values = colors_main_umap) +
+        ggplot2::scale_color_manual(values = colors_main_umap) +
         ggrepel::geom_label_repel(data = labels_all,
                                   x = labels_all$u1,
                                   y = labels_all$u2,
@@ -196,7 +197,7 @@ server <- function(input, output) {
     })
 
   output$Multiplot <-
-    renderPlot({
+    shiny::renderPlot({
       if(input$type_of_data == "Overview") {
         raw_ngene_mt(input_metadata = "/home/jason/data/shiny_dashboard/heart10x/data/metadata_raw_object.csv")
         }
@@ -207,13 +208,12 @@ server <- function(input, output) {
 
       } else if(input$type_of_data == "iris") {
 
-        ggplot2::ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
-          geom_point()
+        ggplot2::ggplot(data = iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+          ggplot2::geom_point()
 
       }
 
-      })
-
+    })
 }
 
-shinyApp(ui, server)
+shiny::shinyApp(ui, server)
