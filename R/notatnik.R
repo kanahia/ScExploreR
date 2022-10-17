f18f5387c852bbcfa8f67b2cd237c0e7406ba1dd
Migdal <mcjmigdal@gmail.com>
2022-10-17 14:12:10 +0200

    add full package::function references to myFeatureplot

--- R/notatnik.R
+++ R/notatnik.R
@@ -23,6 +23,13 @@ one_theme <- function() {
 #' @param label to plot labels or not
 #' @param order whether sort plotting order or not
 #'
+#' @importFrom dplyr rename group_by left_join inner_join select summarise
+#' @importFrom ggplot2 aes ggplot geom_point theme_classic
+#' @import Matrix
+#' @importFrom rlang sym
+#' @importFrom tibble rownames_to_column
+#'
+#' @export
 my_FeaturePlot <- function(metadata,
                            data_slot,
                            gene,
@@ -52,7 +59,7 @@ my_FeaturePlot <- function(metadata,
     dplyr::rename("slot_data" = 2)
 
   metadata <- metadata %>%
-    left_join(., filtered_data_positive,
+    dplyr::left_join(., filtered_data_positive,
               by = c("cell" = "cell"))
 
   metadata$slot_data <- ifelse(is.na(metadata$slot_data), 0, metadata$slot_data)
@@ -64,61 +71,68 @@ my_FeaturePlot <- function(metadata,
   #coorinates for labels
   position_label <-
     metadata %>%
-    dplyr::select(!!sym(identity), UMAP_1, UMAP_2) %>%
-    group_by(!!sym(identity)) %>%
-    summarise(u1 = median(UMAP_1),
-              u2 = median(UMAP_2)) %>%
+    dplyr::select(!!rlang::sym(identity), UMAP_1, UMAP_2) %>%
+    dplyr::group_by(!!rlang::sym(identity)) %>%
+    dplyr::summarise(
+        u1 = median(UMAP_1),
+        u2 = median(UMAP_2)) %>%
     as.data.frame()
 
   FT.plot <-
-    ggplot() +
-    geom_point(data = metadata[metadata$color == "negative", ],
-               aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
-    geom_point(data = metadata[metadata$color != "negative", ],
-               aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
+    ggplot2::ggplot() +
+    ggplot2::geom_point(
+        data = metadata[metadata$color == "negative", ],
+        ggplot2::aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
+    ggplot2::geom_point(
+        data = metadata[metadata$color != "negative", ],
+        ggplot2::aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
     #scale_color_manual(values = c("positive" = "#414487FF" , "negative" = "#FDE725FF")) +
-    theme_classic() +
-    ggtitle(gene) +
+    ggplot2::theme_classic() +
+    ggplot2::ggtitle(gene) +
     one_theme()
 
   if(order == TRUE) {
     FT.plot <-
-      ggplot() +
-      geom_point(data = metadata[metadata$color == "negative", ],
-                 aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
-      geom_point(data = metadata[metadata$color != "negative", ],
-                 aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
+      ggplot2::ggplot() +
+      ggplot2::geom_point(
+        data = metadata[metadata$color == "negative", ],
+        ggplot2::aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
+      ggplot2::geom_point(
+        data = metadata[metadata$color != "negative", ],
+        ggplot2::aes(x = UMAP_1 , y = UMAP_2, color = slot_data), alpha = 0.9, size = 0.04) +
       #scale_color_manual(values = c("positive" = "#414487FF" , "negative" = "#FDE725FF")) +
-      theme_classic() +
-      ggtitle(gene) +
+      ggplot2::theme_classic() +
+      ggplot2::ggtitle(gene) +
       one_theme()
     } else {
       set.seed(42)
       shuffled_metadata <- metadata[sample(1:nrow(metadata)), ]
       FT.plot <-
-        ggplot() +
-        geom_point(data = shuffled_metadata,
-                   aes(x = UMAP_1 , y = UMAP_2, color = slot_data, fill = color), alpha = 0.9, size = 0.04) +
-        theme_classic() +
-        ggtitle(gene) +
+        ggplot2::ggplot() +
+        ggplot2::geom_point(data = shuffled_metadata,
+                   ggplot2::aes(x = UMAP_1 , y = UMAP_2, color = slot_data, fill = color), alpha = 0.9, size = 0.04) +
+        ggplot2::theme_classic() +
+        ggplot2::ggtitle(gene) +
         one_theme() +
-        scale_fill_discrete(guide = "none")
+        ggplot2::scale_fill_discrete(guide = "none")
       }
+
   # plot umap
   if(label == TRUE) {
     FT.plot <-
       FT.plot +
-      annotate(geom = "text",
-               label = position_label[[cluster]],
+      ggplot2::annotate(geom = "text",
+               label = position_label[[identity]],
                x = position_label$u1,
                y = position_label$u2,
                size = 4.5) +
-      scale_color_viridis(direction = -1)
+      viridis::scale_color_viridis(direction = -1)
     } else {
       FT.plot <-
         FT.plot +
-        scale_color_viridis(direction = -1)
+        viridis::scale_color_viridis(direction = -1)
       }
+
   return(FT.plot)
 }
 
