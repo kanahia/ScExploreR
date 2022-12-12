873d0ea33a8f6aa5e5438a1b97cd618d5f569ae6
kanahia <kanahia@iimcb.gov.pl>
2022-12-12 18:17:40 +0100

    restored working app by improving enrichment panel

--- R/enrichment_analysis.R
+++ R/enrichment_analysis.R
@@ -7,8 +7,7 @@
 enrichment_analysis_UI <- function(id, 
                                    label = "Paste your input genes here",
                                    info = "Please, upload your list of genes for gene set enrichment analysis",
-                                   placeholder =  "myh6\ncmlc2\nisl1\ntbx2a\n..."
-                                   ){
+                                   placeholder =  "myh6\ncmlc2\nisl1\ntbx2a\n..."){
   ns <- shiny::NS(id)
   
   shiny::wellPanel(
@@ -28,7 +27,8 @@ enrichment_analysis_UI <- function(id,
           value = "", 
           placeholder = placeholder
         ),
-        shiny::submitButton(text = "Submit", 
+        shiny::actionButton(inputId = ns("button"),
+                            label = "Submit", 
                             icon = shiny::icon("file-upload", lib = "glyphicon"), 
                             width = NULL)
       ),
@@ -50,43 +50,42 @@ enrichment_analysis_Shiny <- function(id) {
   shiny::moduleServer(
     id,
     function(input, output, session) {
-      
-      observeEvent(input$caption, {
-      
-      output$text_output <- shiny::renderPlot({
-
-        out_df <- 
-          data.frame("gene" =
-                       unlist(
-                         stringr::str_split(string = input$caption,
-                                            pattern = "\\\n"))) %>%
-            dplyr::left_join(., drerio_mart_ids, by = c("gene" = "Gene name")) %>%
-            dplyr::select(NCBI) %>%
-            tidyr::drop_na()
-
-        out <-
-          clusterProfiler::enricher(
-            gene = out_df$NCBI,
-            pvalueCutoff = 0.05,
-            pAdjustMethod = "BH",
-            minGSSize = 10,
-            maxGSSize = 500,
-            qvalueCutoff = 0.2,
-            TERM2GENE = term2gene,
-            TERM2NAME = NA
-          )
-        
-        if(!is.null(out)) {
-          enrichplot::dotplot(out) +
-            ggplot2::geom_point(inherit.aes = TRUE, border = "black", alpha = 0.8)+
-            one_theme() +
-            ggplot2::scale_alpha(range = c(0.2, 0.8))
-        } else {print("Your output will appear here:")}
-   
-      }) #render
-    }) #obseevent
+      click <- 
+        eventReactive(
+          eventExpr = input$button,
+          valueExpr = {
+            out_df <- 
+              data.frame("gene" =
+                           unlist(
+                             stringr::str_split(string = input$caption,
+                                                pattern = "\\\n"))) %>%
+              dplyr::left_join(., drerio_mart_ids, by = c("gene" = "Gene name")) %>%
+              dplyr::select(NCBI) %>%
+              tidyr::drop_na()
+            
+            out <-
+              clusterProfiler::enricher(
+                gene = out_df$NCBI,
+                pvalueCutoff = 0.05,
+                pAdjustMethod = "BH",
+                minGSSize = 10,
+                maxGSSize = 500,
+                qvalueCutoff = 0.2,
+                TERM2GENE = term2gene,
+                TERM2NAME = NA)
+            
+            enrichplot::dotplot(out) +
+              ggplot2::geom_point(inherit.aes = TRUE, 
+                                  border = "black", 
+                                  alpha = 0.8)+
+              one_theme() +
+              ggplot2::scale_alpha(range = c(0.2, 0.8)) +
+              viridis::scale_color_viridis()
+          })
+    
+        output$text_output <- shiny::renderPlot({
+          click()
+        }) #render
     } #function
   ) #module
 }
-
-
