1f790d8f67e817119f3b7461f3d4a4ece3b4e482
kanahia <kanahia@iimcb.gov.pl>
2023-06-20 23:00:53 +0000

    export mart_zfin and drerio_mart_ids to NAMESPACE

--- data-raw/drerio_mart_ids.R
+++ data-raw/drerio_mart_ids.R
@@ -1,8 +1,10 @@
 ## code to prepare `drerio_mart_ids` dataset goes here
 
+#' @export
 mart_zfin <- 
   data.table::fread("/home/jason/data/references/mart_export_zfin_ensembl.txt") 
 
+#' @export
 drerio_mart_ids <-
   data.table::fread("/home/jason/data/references/mart_drerio_ids.txt") %>%
   dplyr::rename(NCBI = `NCBI gene (formerly Entrezgene) ID`) %>% 
